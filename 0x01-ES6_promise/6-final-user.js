abe7a6c3e79197314f8f511865d5fc2ef685e506
derrick <boegaderrick@gmail.com>
2024-12-21 00:52:40 +0300

    add task '6. Handle Multiple Promises' files

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => [results[0], { status: results[1].status, value: results[1].reason }]);
 }
