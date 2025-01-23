f72bfea29c3e2e5e5df81146f19031a87647d763
derrick <boegaderrick@gmail.com>
2025-01-23 16:26:21 +0300

    add task '2-Filter' files

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getStudentsByLocation(students, location) {
+  if (!(students instanceof Array)) {
+    return [];
+  }
+  return students.filter((student) => student.location === location);
+}
